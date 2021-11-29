214c5610d3cd21e2a5720a394d384751bd065d6a
GeronimoBarre <geronimobarre500@gmail.com>
2021-11-29 20:53:32 -0300

    moar knowledge

---
+++ ConsoleCalc/ConsoleCalc/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleCalc
+{
+    class Program
+    {
+        static float RealizarOperacion(float primerNumero, float segundoNumero, string operador)
+        {
+            switch (operador)
+            {
+                case "+": return primerNumero + segundoNumero;
+                case "-": return primerNumero - segundoNumero;
+                case "*": return primerNumero * segundoNumero;
+                case "/": return primerNumero / segundoNumero;
+                case "%": return primerNumero % segundoNumero;
+                default: return 0f;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe primer numero");
+            var primerNumeroString = Console.ReadLine();
+            float primerNumero;
+
+            bool esPrimerNumeroValido = float.TryParse(primerNumeroString, out primerNumero);
+
+            if(!esPrimerNumeroValido)
+            {
+                Console.WriteLine("Primer numero invalido");
+                return;
+            }
+
+            Console.WriteLine("Informe segundo numero");
+            var segundoNumeroString = Console.ReadLine();
+            float segundoNumero;
+
+            bool esSegundoNumeroValido = float.TryParse(segundoNumeroString, out segundoNumero);
+
+            if (!esSegundoNumeroValido)
+            {
+                Console.WriteLine("Segundo numero invalido");
+                return;
+            }
+
+            Console.WriteLine("Informe operador");
+            var operador = Console.ReadLine();
+
+            float resultado = RealizarOperacion(primerNumero, segundoNumero, operador);
+            Console.WriteLine("Resultado: " + resultado);
+        }
+
+    }
+}
---
+++ Lista de invitados/Lista de invitados/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Lista_de_invitados
+{
+    class Program
+    {
+        private static string[] ListaDeInvitados = {
+
+        };
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe el nombre del invitado:");
+            var nombre = Console.ReadLine();
+
+            if (nombre == null || string.IsNullOrEmpty(nombre))
+            {
+                Console.WriteLine("El nombre no ha sido informado");
+                return;
+            }
+
+            Console.WriteLine("Informe la edad del invitado");
+            var edadString = Console.ReadLine();
+            int edad;
+
+            bool edadInformada = int.TryParse(edadString, out edad);
+
+            if(edadInformada == false)
+            {
+                Console.WriteLine("Edad no informada o invalida");
+                return;
+            }
+
+            bool estaInvitado;
+            switch (nombre)
+            {
+                case "Geronimo": //0
+                    estaInvitado = true;
+                    break;
+                case "Alejandro":
+                    estaInvitado = true;
+                    break;
+                case "Tu Mama":
+                    estaInvitado = true;
+                    break;
+                case "Freeman":
+                    estaInvitado = true;
+                    break;
+                case "Tyrone":
+                    estaInvitado = true;
+                    break;
+                case "Johnny":
+                    estaInvitado = true;
+                    break;
+                case "Dude":
+                    estaInvitado = true;
+                    break;
+                default:
+                    estaInvitado = false;
+                    break;
+            }
+
+            if(estaInvitado == true && edad > 18)
+            {
+                Console.WriteLine("Bienvendido");
+            }
+            else if(estaInvitado == false)
+            {
+                Console.WriteLine("No esta invitado");
+            }
+            else
+            {
+                Console.WriteLine("Edad no satisfactoria");
+            }
+        }
+    }
+}
