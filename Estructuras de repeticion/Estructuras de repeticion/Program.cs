df590bd35eccffcec4b66546e61b2e8417e42637
GeronimoBarre <geronimobarre500@gmail.com>
2021-12-01 19:24:04 -0300

    another day another code

---
+++ Estructuras de repeticion/Estructuras de repeticion/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Estructuras_de_repeticion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Console.WriteLine("Informar palabra argumento");
+                return;
+            }
+            //for
+            for(var indice = 0; indice < args.Length; indice++)
+            {
+                Console.WriteLine($"Estructura fue, indice y valor: {indice}, {args[indice]}");
+            }
+
+            //while
+            var argumentoLeido = 0;
+             while(argumentoLeido < args.Length)
+            {
+                Console.WriteLine($"Estructura while, argumentosLeidos y valor leido: {argumentoLeido}, {args[argumentoLeido]}"); argumentoLeido++;
+            }
+
+            //do while
+            var loopefectuado = 0;
+            do
+            {
+                Console.WriteLine($"Estructura do while, loopefectuado {loopefectuado} y valor leido {args[loopefectuado]}");
+                loopefectuado++;
+            } while (loopefectuado < args.Length);
+
+            //foreach
+            foreach(var arg in args)
+            {
+                Console.WriteLine($"Estructura foreach, valor leido {arg}");
+            }
+        }
+    }
+}
---
+++ ListaDeProductos/ListaDeProductos/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListaDeProductos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //crear lista de productos que nuestro mercado tiene
+            List<string> productosDisponibles = new List<string>()
+            {
+                "pan", "leche", "manteca", "queso", "gaseosa", "arroz", "lechuga", "tomate", "pollo", "dulce"
+            };
+            //validar si los argumentos fueron completados
+            try
+            {
+                //listar cuales productos informados del argumento hay en la lista del mercado
+                var productosSeleccionados = productosDisponibles.Where(producto => args.Contains(producto)).ToList();
+                foreach(var productoSeleccionado in productosSeleccionados)
+                {
+                    Console.WriteLine($"Este producto tenemos: {productoSeleccionado}");
+                }
+                //listar cuales productos informados no estan disponibles
+                var productosNoDisponibles = args.Where(args => !productosDisponibles.Contains(args)).ToList();
+                foreach(var productoNoDisponible in productosNoDisponibles)
+                {
+                    Console.WriteLine($"Este producto no tenemos: {productoNoDisponible}");
+                }
+                //devolver la lista de productos disponibles del mercado ordenados alfabeticamente
+                var productosDisponiblesOrdenadosAlfabeto = productosDisponibles.OrderBy(e => e).ToList();
+                foreach(var productoOrdenado in productosDisponiblesOrdenadosAlfabeto)
+                {
+                    Console.WriteLine($"Productos: {productoOrdenado}");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Argumentos invalidos");
+            }
+
+        }
+    }
+}
